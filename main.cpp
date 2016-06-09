bfedc36982c16d062954ddd772ec078fe050bb52
Ben Ford <ben.ford@puppetlabs.com>
2016-06-09 09:23:35 -0700

    Updated command line args and added docs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sys/stat.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -92,25 +93,65 @@ class Brainfuck {
     }
 };
 
-int main(int argc, const char* argv[]) {
+void usage() {
+  cout << "Compile a string or file of Brainfuck code."  << endl << endl;
+  cout << "Usage: brainfuck -h | -e <string> | filename" << endl << endl;
+
+  cout << "    <filename>   Compile and run a source file."                << endl;
+  cout << "    -e <string>  Directly evaluate a string as Brainfuck code." << endl;
+  cout << "    -h           Print this usage."                             << endl << endl;
+}
+
+int main(int argc, char **argv) {
   std::ifstream file;
   int length;
+  int opt;
+  char* filename;
   char* buffer;
   struct stat stats;
 
-  if( stat (argv[1], &stats) == 0 ) {
-    file.open(argv[1]);
-    file.seekg(0, std::ios::end);
-    length = file.tellg();
-    file.seekg(0, std::ios::beg);
-    buffer = new char[length];
-    file.read(buffer, length);
-    file.close();
+  opterr = 0;
+  while ((opt = getopt (argc, argv, "he:")) != -1) {
+    switch (opt) {
+      case 'h':
+        usage();
+        exit(0);
+
+      case 'e':
+        buffer = (char*) optarg;
+        break;
+
+      case '?':
+        if (optopt == 'e')
+          fprintf (stderr, "Option '-%c' requires an argument.\n", optopt);
+        else
+          fprintf (stderr, "Unknown option '-%c'.\n", optopt);
+        exit(1);
+
+      default:
+        exit(2);
+      }
   }
-  else {
-    buffer = (char*) argv[1];
+
+  if(! buffer) {
+    // not run with -e, lets look for a file
+    filename = (char*) argv[optind];
+
+    if( stat (filename, &stats) == 0 ) {
+      file.open(filename);
+      file.seekg(0, std::ios::end);
+      length = file.tellg();
+      file.seekg(0, std::ios::beg);
+      buffer = new char[length];
+      file.read(buffer, length);
+      file.close();
+    }
+    else {
+      fprintf (stderr, "Cannot open file %s.\n", filename);
+      exit(2);
+    }
   }
-  
+
   Brainfuck interpreter = Brainfuck(buffer);
   interpreter.evaluate();
 }
